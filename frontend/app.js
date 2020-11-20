d7e967e0e83009b834a37cbeb8aa09b7ac77e7a2
edkrueger <edkrueger@gmail.com>
2020-11-19 20:12:17 -0600

    Have the frontend use the deployed route.

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,4 @@
-//  from the json
-
-d3.json("owid-covid-2020-11-19-usa.json").then(
+d3.json("https://p6w-3-sep-full-stack.herokuapp.com/data").then(
     data => {
         let trace1 = {
             x: data.map(d => d.date),
@@ -29,35 +27,3 @@ d3.json("owid-covid-2020-11-19-usa.json").then(
         Plotly.newPlot("myPlot", plotData, plotLayout)
     }
 )
-
-// from csv (d3.csv translates the csv into a json for us!)
-
-// d3.csv("owid-covid-2020-11-19-usa.csv").then(
-//     data => {
-//         let trace1 = {
-//             x: data.map(d => d.date),
-//             y: data.map(d => d.total_tests),
-//             name: "Total Tests"
-//         }
-
-//         let trace2 = {
-//             x: data.map(d => d.date),
-//             y: data.map(d => d.total_cases),
-//             name: "Total Cases"
-//         }
-
-//         let trace3 = {
-//             x: data.map(d => d.date),
-//             y: data.map(d => d.total_deaths),
-//             name: "Total Deaths"
-//         }
-
-//         let plotData = [trace1, trace2, trace3]
-
-//         let plotLayout = {
-//             title: "USA COVID"
-//         }
-
-//         Plotly.newPlot("myPlot", plotData, plotLayout)
-//     }
-// )
